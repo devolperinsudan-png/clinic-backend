bac6733002b8ce8e5e3712361260a53bdb8241ed
devolperinsudan-png <devolperinsudan@gmail.com>
2026-09-24 23:52:08 +0200

    server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/', (req, res) => {
     res.send('🚀 Digital Clinic Backend API is live and working smoothly!');
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Server is running securely on port ${PORT}`);
 });
